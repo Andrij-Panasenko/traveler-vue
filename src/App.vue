c818a4d94ff14085c4a42eea3b5f919377887e11
VladProtchenko <andrii.panasenko295@gmail.com>
2024-08-06 21:37:45 +0300

    FavoritePlaces

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 import HomepageView from './views/HomepageView.vue'
 </script>
 
 <template>
+  <div class="bg-white h-screen w-[400px]">
+    <FavoritePlaces />
+  </div>
   <HomepageView />
 </template>
